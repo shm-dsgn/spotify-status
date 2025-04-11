565de994c89743779b2e87fd8611787a7dab61f9
shm-dsgn <77527904+shm-dsgn@users.noreply.github.com>
2025-04-11 21:27:11 +0530

    Added links

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
 import getNowPlayingItem from "@/components/SpotifyStatus/SpotifyAPI";
 import SpotifyStatus from "@/components/SpotifyStatus/SpotifyStatus";
+import Link from "next/link";
 
 export default async function Home() {
   const initialData = await getNowPlayingItem();
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <SpotifyStatus initialData={initialData}/>
+    <div className="mx-4 flex flex-col gap-12">
+      <SpotifyStatus initialData={initialData} />
+      <div className="flex flex-col gap-4">
+        <Link
+          href={"https://github.com/shm-dsgn/spotify-status"}
+          target="_blank"
+          className="text-xs underline text-neutral-400"
+        >
+          Source Code on Github
+        </Link>
+        <Link
+          href={"https://sohamdutta.in"}
+          target="_blank"
+          className="text-xs underline text-neutral-400"
+        >
+          My Portfolio
+        </Link>
+      </div>
     </div>
   );
 }
